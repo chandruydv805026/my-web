814ac084a36b323163fd4f28c4b491a66d3df3aa
chandruydv805026 <ck805026@gmail.com>
2025-10-18 12:48:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Optional: fallback route
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
@@ -84,5 +84,6 @@ mongoose.connect(process.env.DBurl).then(() => {
 });
 
 });
+
 
 
